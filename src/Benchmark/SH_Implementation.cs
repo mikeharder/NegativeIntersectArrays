729398c219af618c096b72b88e95f2cec6f70bb7
Joe Cale <joe.cale@sunhotels.com>
2018-06-04 11:39:23 +0200

    Shave another 0.2ms off :p

--- src/Benchmark/SH_Implementation.cs
+++ src/Benchmark/SH_Implementation.cs
@@ -12,40 +12,38 @@ namespace NegativeIntersectArrays
         //        new DefaultPooledObjectPolicy<int[]>(), 20
         //        );
 
-        static int[] emptyArray = new int[0];
+        static int[] EmptyResult = new int[0];
         public static int[] NegativeIntersectArrays(int[] readonlySource, IReadOnlyList<int[]> exceptions)
         {
             var sourceLength = readonlySource.Length;
             if (sourceLength == 0)
-                return emptyArray;
+                return EmptyResult;
 
             var source = arrayPool.Rent(sourceLength);
-            //var source = new int[sourceLength];
             Buffer.BlockCopy(readonlySource, 0, source, 0, sourceLength * sizeof(int));
 
             int lastWorkingLength = sourceLength;
             var maxSearch = source[sourceLength - 1];
+
             for (var i = 0; i < exceptions.Count; i++)
             {
-                var arr1Len = lastWorkingLength;
                 var arr2Len = exceptions[i].Length;
-                var baseArray = source;
                 var comparedArray = exceptions[i];
-                int baseArrayIndex = 0, comparedArrayIndex = 0;
-                int writeIndex = 0;
+
+                int baseArrayIndex = 0,
+                    comparedArrayIndex = 0,
+                    writeIndex = 0;
 
                 if (comparedArray.Length == 0 || comparedArray[0] > maxSearch)
-                {
-                    continue; // this entire range is out of the range of the first, nothing will be found..
-                }
+                    continue; // this entire (SORTED!) range is out of the range of the first range, nothing will be excluded..
 
-                while (baseArrayIndex < arr1Len && comparedArrayIndex < arr2Len)
+                while (baseArrayIndex < lastWorkingLength && comparedArrayIndex < arr2Len)
                 {
-                    var baseVal = baseArray[baseArrayIndex];
+                    var baseVal = source[baseArrayIndex];
                     var compVal = comparedArray[comparedArrayIndex];
                     if (compVal < baseVal)
                     {
-                        comparedArrayIndex++;
+                        ++comparedArrayIndex;
                     }
                     else if (baseVal == compVal)
                     {
@@ -59,22 +57,16 @@ namespace NegativeIntersectArrays
                     }
                 }
 
-                while (baseArrayIndex < arr1Len) // if the baseArray is longer than the array we have to except, we need to copy in the rest of the items
-                {
-                    source[writeIndex++] = baseArray[baseArrayIndex];
-                    baseArrayIndex++;
-                }
+                while (baseArrayIndex < lastWorkingLength) // if the baseArray is longer than the array we have to except, we need to copy in the rest of the items
+                    source[writeIndex++] = source[baseArrayIndex++];
+
                 lastWorkingLength = writeIndex;
             }
-            //return new Span<int>(source, 0, lastWorkingLength).ToArray();
             var returnable = new int[lastWorkingLength];
             Buffer.BlockCopy(source, 0, returnable, 0, sizeof(int) * lastWorkingLength);
             arrayPool.Return(source);
 
             return returnable;
-            //Array.Copy()
-            //Array.Resize(ref source, lastWorkingLength);
-            //return source;
         }
     }
 }
